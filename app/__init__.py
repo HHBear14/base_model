d22030f70a0802f6ef2b600f39fa88cf35498ebd
Unknown <mdovturner@gmail.com>
2018-03-23 14:48:29 -0500

    set up connection with MySQL and create user form and model

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask_login import LoginManager
+from flask import Flask
+from config import Config
+
+
+app2 = Flask(__name__)
+app2.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy(app2)
+migrate = Migrate(app2, db)
+
+login = LoginManager(app2)
+
+from app import routes
--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template
+from app import app2
 
-app=Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-app.run()
+app2.run()
